ccf9f7e40e017a1ace9a3381e157c000550ed833
Pritam Kumar <neilpritamk@gmail.com>
2023-02-08 00:58:50 +0530

    implemented polymorphism as directed in the question

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
+    public static void main( String args[]){
+        RWOnly obj=new RWOnly();
+//        obj.name="pritam";
+//        System.out.println(obj.name);
+        // java: name has private access in com.driver.RWOnly
+        obj.setName("pritam");
+        obj.getName();
+    }
   
 }
